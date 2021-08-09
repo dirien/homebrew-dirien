383ceb74b34016b292d3247f1e9003934d525d7b
dirien <engin.diri@mail.schwarz>
2021-08-09 07:42:46 +0200

    Brew formula update for minectl version v0.7.0

--- Formula/minectl.rb
+++ Formula/minectl.rb
@@ -5,29 +5,29 @@
 class Minectl < Formula
   desc "minectl️️ 🗺️ is a cli for creating Minecraft (java or bedrock) server on different cloud provider."
   homepage "https://github.com/dirien/minectl"
-  version "0.6.1"
+  version "0.7.0"
   license "Apache License 2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dirien/minectl/releases/download/v0.6.1/minectl_0.6.1_darwin_amd64.tar.gz"
-      sha256 "3a4a2ca51d92dee69bb6d98037e7ef30136d812a4b0db6176eeec5d861e1b231"
+      url "https://github.com/dirien/minectl/releases/download/v0.7.0/minectl_0.7.0_darwin_amd64.tar.gz"
+      sha256 "df6ad4c88b282847214278e09d43c8a8c6e85b1003730e35e7b319b20ab4a3a8"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/dirien/minectl/releases/download/v0.6.1/minectl_0.6.1_darwin_arm64.tar.gz"
-      sha256 "c944c5caff27a0a653b7c07adc8e914340eedb3558e25d8ffe5a8bbd9eafad11"
+      url "https://github.com/dirien/minectl/releases/download/v0.7.0/minectl_0.7.0_darwin_arm64.tar.gz"
+      sha256 "20ea72bf308218ef2656017e3e028aba3886ed81f90616009e6e903f1a2db8b7"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/dirien/minectl/releases/download/v0.6.1/minectl_0.6.1_linux_amd64.tar.gz"
-      sha256 "6f56d8ed26c2faa32df92f65436ab07f67763b6b0cba3664d20f14d9e19a0a1d"
+      url "https://github.com/dirien/minectl/releases/download/v0.7.0/minectl_0.7.0_linux_amd64.tar.gz"
+      sha256 "2158e4d829c2fd30ff20ead830736b8d227afbbbff245211f554f9b8448934c4"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dirien/minectl/releases/download/v0.6.1/minectl_0.6.1_linux_arm64.tar.gz"
-      sha256 "c84478c6f02362018c70472d6b523c198956a817ca5259ac72963e14ee4deebc"
+      url "https://github.com/dirien/minectl/releases/download/v0.7.0/minectl_0.7.0_linux_arm64.tar.gz"
+      sha256 "015dafba89ff5b0a402fc791bcad92b7f7d51a557307678a32cef2146bfe4d4f"
     end
   end
 
