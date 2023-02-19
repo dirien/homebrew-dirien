004502f9f33314c576c818b22b6bbc5b6c8945eb
dirien <engin.diri@mail.schwarz>
2023-02-19 09:28:47 +0100

    Brew formula update for tfu version v0.5.0

--- Formula/tfu.rb
+++ Formula/tfu.rb
@@ -5,21 +5,21 @@
 class Tfu < Formula
   desc "tfu is a Terraform helper to update the providers."
   homepage "https://github.com/dirien/tfu"
-  version "0.4.2"
+  version "0.5.0"
   license "Apache License 2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dirien/tfu/releases/download/v0.4.2/tfu_0.4.2_darwin_amd64.tar.gz"
-      sha256 "4af54767880a87c1d46ccbf506c4af0f84c5893d514b04fb3f0cca1f540a8894"
+      url "https://github.com/dirien/tfu/releases/download/v0.5.0/tfu_0.5.0_darwin_amd64.tar.gz"
+      sha256 "2a67bec614c803becfbd0b2179685eda5c34569545e170cc8ea93fa3fdf1a7d9"
 
       def install
         bin.install "tfu"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/dirien/tfu/releases/download/v0.4.2/tfu_0.4.2_darwin_arm64.tar.gz"
-      sha256 "65f09922bf3fee00f2244973f165f7b6b51c06b695d87b616287700ce719c539"
+      url "https://github.com/dirien/tfu/releases/download/v0.5.0/tfu_0.5.0_darwin_arm64.tar.gz"
+      sha256 "d3e29a39846d34fdd2b47ac0bc072503a0d6031e692eb1107a094179b391135d"
 
       def install
         bin.install "tfu"
@@ -29,16 +29,16 @@ class Tfu < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/dirien/tfu/releases/download/v0.4.2/tfu_0.4.2_linux_amd64.tar.gz"
-      sha256 "85d9d2822c043053fcbfc71b24da2c1e302302936498b5533cb14710bc136e72"
+      url "https://github.com/dirien/tfu/releases/download/v0.5.0/tfu_0.5.0_linux_amd64.tar.gz"
+      sha256 "caef9655cb0b84819a4e6209f3568a1632c1d0d58cd07b520cdc0b08dfa21de0"
 
       def install
         bin.install "tfu"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dirien/tfu/releases/download/v0.4.2/tfu_0.4.2_linux_arm64.tar.gz"
-      sha256 "1b52c5eaa70f1f4014c59f2f5c85ec2efb33e0e61983e265d35fbb81d27eb77f"
+      url "https://github.com/dirien/tfu/releases/download/v0.5.0/tfu_0.5.0_linux_arm64.tar.gz"
+      sha256 "8e83b7e22fe0f8641a19d01537ca272a5a519858f659157fab6542980fe7604e"
 
       def install
         bin.install "tfu"
